69c87f4bd6c42d907be1406e393a2ab038eea51c
hiroakissh <hiroaki.saito1014@gmail.com>
2024-02-04 10:24:17 +0900

    [add] project

---
+++ StableDiffusionSapmple/StableDiffusionSapmpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StableDiffusionSapmpleApp.swift
+//  StableDiffusionSapmple
+//
+//  Created by HiroakiSaito on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct StableDiffusionSapmpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
